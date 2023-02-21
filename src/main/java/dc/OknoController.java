d6d03f6a226aea2b88ce538463e19a87305f5562
darecc <dariusz.ceglarek@gmail.com>
2023-02-21 21:33:58 +0100

    Second version: save to file, read from file etc.

--- src/main/java/dc/OknoController.java
+++ src/main/java/dc/OknoController.java
@@ -3,20 +3,21 @@ package dc;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import dc.OsobaFx.*;
 
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class OknoController implements Initializable {
+    private final String FILENAME = "osoby.txt";
     @FXML
-    Button login;
+    Button bLogin, bDodaj, bWczytaj,bZapisz;;
     @FXML
     TextField tLogin;
     @FXML
@@ -32,11 +33,9 @@ public class OknoController implements Initializable {
     @FXML
     AnchorPane anchorPane;
     @FXML
-    Tab tabTabele;
+    Tab tabTabele, tabWykresy;
     @FXML
     ImageView image;
-    @FXML
-    Button bDodaj;
     @FXML
     TableView<OsobaFx> tabOsoby;
     @FXML
@@ -51,10 +50,29 @@ public class OknoController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         tabTabele.setDisable(true);
+        tabWykresy.setDisable(true);
         cImie.setCellValueFactory(new PropertyValueFactory<OsobaFx, String>("Imie"));
         cNazwisko.setCellValueFactory(new PropertyValueFactory<OsobaFx, String>("Nazwisko"));
         cTelefon.setCellValueFactory(new PropertyValueFactory<OsobaFx, String>("Telefon"));
-        //cEmail.setCellValueFactory(new PropertyValueFactory<OsobaFx, String>("Email"));
+        cEmail.setCellValueFactory(new PropertyValueFactory<OsobaFx, String>("Email"));
+        //region stylizacja kontrolek
+        String styl1 = "-fx-background-color: #D7BCC9; -fx-color: #EE7C00; -fx-margin: 10px;";
+        String styl2 = "-fx-background-color: #D78CA9; -fx-color: #33ACBB; -fx-margin: 10px;";
+        String styl3 = "-fx-background-color: #3c7fb1,\n" +
+                "        linear-gradient(#fafdfe, #e8f5fc),\n" +
+                "        linear-gradient(#eaf6fd 0%, #d9f0fc 49%, #bee6fd 50%, #a7d9f5 100%);\n" +
+                "    -fx-background-insets: 0,1,2;\n" +
+                "    -fx-background-radius: 3,2,1;\n" +
+                "    -fx-padding: 3 20 3 20;\n" +
+                "    -fx-text-fill: black;\n" +
+                "    -fx-font-size: 14px;";
+        //endregion
+        bDodaj.setStyle(styl3);
+        bWczytaj.setStyle(styl3);
+        bZapisz.setStyle(styl3);
+        tabTabele.setStyle(styl1);
+        bDodaj.setTooltip(new Tooltip("Dodanie nowej osoby do tabeli znajomych"));
+        bZapisz.setTooltip(new Tooltip("Zapisanie listy znajomych do pliku"));
     }
 
     @FXML
@@ -63,17 +81,84 @@ public class OknoController implements Initializable {
         String passw = tPassword.getText();
         if (login.equals("Darek") && passw.equals("1234")) {
             tabTabele.setDisable(false);
+            tabWykresy.setDisable(false);
         }
     }
 
     @FXML
-    private void DodajDoTabeli(ActionEvent ae) {
-        String i = tImie.getText();
-        String n = tNazwisko.getText();
-        String t = tTelefon.getText();
-        String e = tEmail.getText();
-        //OsobaFx osoba = new OsobaFx(i, n, t, e);
-        OsobaFx osoba = new OsobaFx(i, n, t);
+    private void doDodaj(ActionEvent ae) {
+        String imie = tImie.getText();
+        String nazw = tNazwisko.getText();
+        String tele = tTelefon.getText();
+        String emai = tEmail.getText();
+        OsobaFx osoba = new OsobaFx(imie, nazw, tele, emai);
         tabOsoby.getItems().add(osoba);
+    }
+
+    /**
+     * Zapis danych z tabeli 'tabOsoby' do pliku
+     * @param ae
+     */
+    @FXML
+    private void doZapisz(ActionEvent ae) {
+        try {
+            FileWriter fw = new FileWriter(FILENAME);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for(OsobaFx osoba : tabOsoby.getItems()) {
+                bw.write(osoba.getImie() + ";" + osoba.getNazwisko() + ";" + osoba.getTelefon() + ";" + osoba.getEmail());
+                bw.newLine();
+            }
+            bw.close();
+            fw.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @FXML
+    private void doWczytaj(ActionEvent ae) {
+        boolean jest = sprawdzCzyPlikIstnieje(FILENAME);
+        if (jest == false) {
+            String ret = showMessage("Nie ma pliku: " + FILENAME);
+        }
+        tabOsoby.getItems().clear(); // oczyszczenie dotychczasowej zawartości tabeli
+        try {
+            FileReader fr = new FileReader(FILENAME);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] fragments = line.split(";");
+                OsobaFx osoba = new OsobaFx(fragments[0],fragments[1],fragments[2],fragments[3]);
+                tabOsoby.getItems().add(osoba);
+            }
+        }
+        catch(IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    private boolean sprawdzCzyPlikIstnieje(String fileName) {
+        File f = new File(fileName);
+        if (f.exists())
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Wyświetlenie komunikatu w okienku alertu
+     * @param message
+     */
+    private String showMessage(String message) {
+        AtomicReference<String> msgRet = null;
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Komunikat");
+        alert.setHeaderText("");
+        alert.setContentText(message);
+        alert.showAndWait().ifPresent(rs -> {
+            if (rs == ButtonType.OK) {
+                msgRet.set("OK");
+            }
+        });
+        return "OK";
     }
 }
--- src/main/java/dc/OsobaFx.java
+++ src/main/java/dc/OsobaFx.java
@@ -14,12 +14,7 @@ public class OsobaFx {
             this.telefon = new SimpleStringProperty(telefon);
             this.email = new SimpleStringProperty(email);
         }
-        public OsobaFx(String imie, String nazwisko, String telefon) {
-        this.imie = new SimpleStringProperty(imie);
-        this.nazwisko = new SimpleStringProperty(nazwisko);
-        this.telefon = new SimpleStringProperty(telefon);
-        this.email = new SimpleStringProperty("aa");
-    }
+
         public OsobaFx getAccount() {
             return this;
         }
@@ -45,5 +40,17 @@ public class OsobaFx {
         public String toString() {
             return imie + " " + nazwisko + " (" + telefon + ")";
         }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
 }
 
